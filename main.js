25921dfb7cefd247b81bd898dbf3d10682c7bd32
Otkir77 <166150681+Otkir77@users.noreply.github.com>
2024-06-22 15:23:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+let a = +prompt('Bitta son kiriting')
+let b = +prompt('Ikkinchi sonni kiriting')
+
+if (a + b) {
+    alert ( a + b + 8 ,)
+} 
+2
+let a_ = +('12')
+console.log(a);
+
+
+3
+
+
+let random_number = Math.floor(Math.random()*100 )
+
+if (random_number){
+    random_number.push(random_number[i])
+}
+
+
+console.log(random_number);  
+
+
+4
+
+let a_2 =prompt('Soz kiriting')
+
+if (a == 'mars'){
+    alert('mars')
+} else {
+    alert('mars it school')
+}
+
+5
+
+let arr = ['Bmw','Mers','Sigir','Buqa','Porsche']
+
+
+
+6
+
+
+let a_3 = []
+for(let i=0; i < 100; i++) {
+    if([i] % 2 == 0){
+        a.push([i])
+    }
+}
+console.log(a, 'juft sonlari');
